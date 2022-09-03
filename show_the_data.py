e0a71981839e8e946e4a85a51650ffb5cba1a922
Hasan Fatih Yıldırgan <fatihyildirgan@hotmail.com>
2022-09-04 01:02:41 +0300

    exercise

---
+++ show_the_data.py
@@ -0,0 +1,13 @@
+# embarked değeri S olmayanların tüm bilgelerini gösteriniz.
+import seaborn as sns
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+#print(df["embarked"])
+
+#a = df[(df['embarked'] == 'C') | (df['embarked'] == 'Q')]
+#b = df[df["embarked"] != 'S']
+print(a)
+print(b)
+
